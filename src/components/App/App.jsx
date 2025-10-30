4dfc9b5fa2f452a95298f92c5495a11d27394e8b
Cedwards <cedwards2527@gmail.com>
2025-10-30 12:06:46 -0400

    Add modal styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -69,7 +69,7 @@ function App() {
           />
         </label>
         <fieldset className="modal__radio-buttons">
-          <legend className="modal__legend">Select the weather type</legend>
+          <legend className="modal__legend">Select the weather type:</legend>
           <label htmlFor="hot" className="modal__label modal__label_type_radio">
             <input id="hot" type="radio" className="modal__radio-input" />
             Hot
