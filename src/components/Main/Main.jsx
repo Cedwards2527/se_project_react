56fff391351e1805ae50751dfb174fc3c184005b
Cedwards <cedwards2527@gmail.com>
2025-11-08 14:55:32 -0500

    Add Current temp unit to Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
 import WeatherCard from "./WeatherCard/WeatherCard";
 import "./Main.css";
 import ItemCard from "./ItemCard/ItemCard";
+import CurrentTemperatureUnitContext from "../../context/CurrentTemperatureUnitContext";
 
 function Main({ weatherData, handleCardClick, clothingItems }) {
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
   return (
     <main className="main">
       <WeatherCard weatherData={weatherData} />
       <section className="cards">
         <p className="cards__text">
-          Today is {weatherData.temp.F} &deg; F / You may want to wear:
+          Today is {weatherData.temp[currentTemperatureUnit]}&deg;{" "}
+          {currentTemperatureUnit} / You may want to wear:
         </p>
         <ul className="cards__list">
           {clothingItems
