69003da92d9528ec3c632047efd875441c4fc81f
Cedwards <cedwards2527@gmail.com>
2025-11-14 11:23:17 -0500

    Add post and delete api

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,3 +10,22 @@ const handleServerResponse = (res) => {
 
 export const getItems = () =>
   fetch(`${baseUrl}/items`, { headers }).then(handleServerResponse);
+
+export const addItem = ({ name, imageUrl, weather }) => {
+  return this._request(`${this._baseUrl}/items`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      name,
+      imageUrl,
+      weather,
+    }),
+  }).then(handleServerResponse);
+};
+
+export const deleteItems = (itemID) => {
+  return fetch(`${this._baseUrl}/items/${itemID}`, {
+    method: "DELETE",
+    headers,
+  }).then(handleServerResponse);
+};
