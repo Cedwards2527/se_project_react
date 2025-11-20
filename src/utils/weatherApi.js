dc4ddc081b10cd202378e74ac8152e8941ab1bf8
Cedwards <cedwards2527@gmail.com>
2025-11-19 23:13:19 -0500

    Update Weather api

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,6 +1,6 @@
-export const getWeather = ({ latitude, longitude }, APIkey) => {
+export const getWeather = ({ latitude, longitude }, apiKey) => {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
   ).then((res) => {
     if (res.ok) {
       return res.json();
@@ -28,7 +28,7 @@ const isDay = ({ sunrise, sunset }) => {
 };
 
 const getWeatherType = (temperature) => {
-  if (temperature > 86) {
+  if (temperature >= 86) {
     return "hot";
   } else if (temperature >= 66 && temperature < 86) {
     return "warm";
