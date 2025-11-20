4b2bafe53995327b5c186a71876adb1b5aec3429
Cedwards <cedwards2527@gmail.com>
2025-11-19 23:00:34 -0500

    Update Modal Button

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -3,10 +3,7 @@ import close from "../../assets/close.svg";
 
 function DeleteModal({ isOpen, onClose, card, handleDeleteItem }) {
   return (
-    <div
-      className={`modal  ${isOpen ? "modal_opened" : ""}`}
-      onClick={onClose}
-    >
+    <div className={`modal  ${isOpen ? "modal_opened" : ""}`} onClick={onClose}>
       <div
         className="modal__container modal__container_type_delete"
         onClick={(e) => e.stopPropagation()}
@@ -22,7 +19,7 @@ function DeleteModal({ isOpen, onClose, card, handleDeleteItem }) {
         <div className="modal__buttons" id="delete-form">
           <button
             type="button"
-            onClick={() => handleDeleteItem(card.id)}
+            onClick={() => handleDeleteItem(card._id)}
             aria-label="Delete image"
             className="mobal__btn mobal__btn_type_delete"
           >
